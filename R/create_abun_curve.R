707bfd049390247830d3e08f8e854404a2b85674
csim063 <simpkinscraig063@gmail.com>
2023-06-17 15:43:08 +1200

    Initial package setup

---
+++ R/create_abun_curve.R
@@ -0,0 +1,12 @@
+#' Create species abundance curve
+#'
+#' @param species A list of species
+#' @param counts Associated counts for each species
+#'
+#' @return A abundance curve ggplot object
+#' @export
+#'
+#' @examples p <- (species, counts)
+create_abun_curve <- function(species, counts){
+  print("this is a test holder")
+}
